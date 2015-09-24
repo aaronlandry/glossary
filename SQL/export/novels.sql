d106f7fe7936185c74f3991ad349f016cdf60bf1
Aaron Landry <alandry@sundell.net>
2015-09-23 22:54:56 -0400

    Export

---
+++ SQL/export/novels.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  File created - Wednesday-September-23-2015   
+--------------------------------------------------------
+REM INSERTING into GLOSSARYAPPLICATION.NOVELS
+SET DEFINE OFF;
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (1,1,1,to_timestamp('21-JUN-15 09.20.59.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) The Shores of Gor''Nahara');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (2,2,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) The Twiceborn');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (3,3,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) The Mephitic Blood');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (4,4,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) In the Wake of Dragons');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (5,5,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) The Thread of Ages');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (6,6,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) The Land of the Dawn');
+Insert into GLOSSARYAPPLICATION.NOVELS (ID,ORDINAL,USERID,CREATIONDATE,NAME) values (8,8,1,to_timestamp('21-JUN-15 09.21.07.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),'(Superiad) Blades of Flesh');
